d3da940d349f0758ac93b7dabebfe3a178cf624d
Thuy Tran Van <thuytravnan.gl@gmail.com>
2025-03-24 22:11:44 +0700

    update

--- plugin/options.lua
+++ plugin/options.lua
@@ -45,7 +45,6 @@ vim.opt.whichwrap:append("<,>,[,],h,l")         -- keys allowed to move to the p
 vim.opt.formatoptions:remove({ "c", "r", "o" }) -- This is a sequence of letters which describes how automatic formatting is to be done
 vim.opt.linebreak = true
 vim.opt.colorcolumn = "80"
-vim.o.shell = "cmd"
 
 if vim.g.neovide then
   vim.g.neovide_remember_window_size = true
