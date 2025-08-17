d134744dc5a4a60728c09c6855fb8b52feeb6888
thuy tran van <thuytranvan.gl@gmail.com>
2025-08-17 11:10:22 +0700

    add neovide config

---
+++ plugin/neovide.lua
@@ -0,0 +1,24 @@
+if vim.g.neovide then
+  vim.g.neovide_fullscreen = true
+  vim.o.guifont = "Source Code Pro:h14" -- text below applies for VimScript
+
+  -- Allow clipboard copy paste in neovim
+  vim.api.nvim_set_keymap("", "<C-v>", "+p<CR>", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("!", "<C-v>", "<C-R>+", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("t", "<C-v>", "<C-R>+", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("v", "<C-v>", "<C-R>+", { noremap = true, silent = true })
+
+  vim.g.neovide_scale_factor = 1.0
+
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+
+  vim.keymap.set("n", "<C-=>", function()
+    change_scale_factor(1.25)
+  end)
+
+  vim.keymap.set("n", "<C-->", function()
+    change_scale_factor(1 / 1.25)
+  end)
+end
