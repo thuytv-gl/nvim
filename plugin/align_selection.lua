4452f39b19c01dad527128d805a3a0415a3ff259
tran van thuy <thuytranvan.gl@gmail.com>
2026-01-03 19:45:23 +0700

    chore: add align selection

---
+++ plugin/align_selection.lua
@@ -0,0 +1,60 @@
+local M = {}
+
+--- @param lines string[]
+--- @param char string
+local function get_max_char_pos(lines, char)
+  local max_pos = 0
+  for _, line in ipairs(lines) do
+    local pos = line:find(char, 1, true) -- plain search for speed
+    if pos and pos > max_pos then
+      max_pos = pos
+    end
+  end
+  return max_pos
+end
+
+--- @param lines string[]
+--- @param char string
+local function align_lines(lines, char)
+  local target_pos = get_max_char_pos(lines, char)
+  if target_pos == 0 then
+    return lines
+  end
+
+  local formatted = {}
+  for i, line in ipairs(lines) do
+    local pos = line:find(char, 1, true)
+    if pos and pos < target_pos then
+      local before = line:sub(1, pos - 1)
+      local after = line:sub(pos)
+      formatted[i] = before .. string.rep(" ", target_pos - pos) .. after
+    else
+      formatted[i] = line
+    end
+  end
+  return formatted
+end
+
+--- @param char string
+function M.align_selection_by_char(char)
+  char = char or "="
+
+  -- Get range and normalize for upwards selection
+  local start_lnum = vim.fn.line("v")
+  local end_lnum = vim.fn.line(".")
+  local start_row = math.min(start_lnum, end_lnum) - 1
+  local end_row = math.max(start_lnum, end_lnum)
+
+  -- Get, Process, and Set
+  local lines = vim.api.nvim_buf_get_lines(0, start_row, end_row, false)
+  local processed_lines = align_lines(lines, char)
+
+  vim.api.nvim_buf_set_lines(0, start_row, end_row, false, processed_lines)
+end
+
+-- Keymap
+vim.keymap.set("v", "-", function()
+  M.align_selection_by_char("=")
+end, { desc = "Align selection by '='" })
+
+return M
--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -3,52 +3,52 @@
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>lq', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set("n", "<leader>lq", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- Better paste
-vim.keymap.set('v', 'p', '"_dP', { silent = true })
+vim.keymap.set("v", "p", '"_dP', { silent = true })
 
 -- Visual --
 -- Stay in indent mode
-vim.keymap.set('v', '<', '<gv', { silent = true })
-vim.keymap.set('v', '>', '>gv', { silent = true })
+vim.keymap.set("v", "<", "<gv", { silent = true })
+vim.keymap.set("v", ">", ">gv", { silent = true })
 
-vim.keymap.set('n', '<C-q>', function()
-  vim.cmd [[
+vim.keymap.set("n", "<C-q>", function()
+  vim.cmd([[
     if empty(filter(getwininfo(), 'v:val.quickfix'))
       copen
     else
       cclose
     endif
-  ]]
-end, { desc = 'Move focus to the upper window' })
+  ]])
+end, { desc = "Move focus to the upper window" })
 
 -- quick exit insert mode
-vim.keymap.set('i', 'jk', '<ESC>', { silent = true })
-vim.keymap.set('i', 'kj', '<ESC>', { silent = true })
+vim.keymap.set("i", "jk", "<ESC>", { silent = true })
+vim.keymap.set("i", "kj", "<ESC>", { silent = true })
 
 -- save and load session
-vim.keymap.set('n', '<C-s><C-s>', '<cmd>mks! ~/session.nvim<CR>')
-vim.keymap.set('n', '<C-s><C-l>', '<cmd>source ~/session.nvim<CR>', { silent = true })
+vim.keymap.set("n", "<C-s><C-s>", "<cmd>mks! ~/session.nvim<CR>")
+vim.keymap.set("n", "<C-s><C-l>", "<cmd>source ~/session.nvim<CR>", { silent = true })
 
 -- buffline
-vim.keymap.set('n', '<S-l>', ':BufferLineCycleNext<CR>', { silent = true })
-vim.keymap.set('n', '<S-h>', ':BufferLineCyclePrev<CR>', { silent = true })
+vim.keymap.set("n", "<S-l>", ":BufferLineCycleNext<CR>", { silent = true })
+vim.keymap.set("n", "<S-h>", ":BufferLineCyclePrev<CR>", { silent = true })
 
 -- save and quit
-vim.keymap.set('n', '<leader>w', '<cmd>w<CR>')
-vim.keymap.set('n', '<leader>q', '<cmd>q!<CR>')
-vim.keymap.set('n', '<leader>ca', '<cmd>%bd|e#|bd#<CR>', { silent = true, desc = "Close others" })
-vim.keymap.set('n', '<leader>c', '<cmd>bp | sp | bn | bd!<CR>')
+vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
+vim.keymap.set("n", "<leader>q", "<cmd>q!<CR>")
+vim.keymap.set("n", "<leader>ca", "<cmd>%bd|e#|bd#<CR>", { silent = true, desc = "Close others" })
+vim.keymap.set("n", "<leader>c", "<cmd>bp | sp | bn | bd!<CR>")
 
-vim.keymap.set({ 'n', 'v' }, '<C-u>', '{')
-vim.keymap.set({ 'n', 'v' }, '<C-d>', '}')
+vim.keymap.set({ "n", "v" }, "<C-u>", "{")
+vim.keymap.set({ "n", "v" }, "<C-d>", "}")
