dac40895e45ca7794df2212e554f085e6c2b7d9a
tran van thuy <thuytranvan.gl@gmail.com>
2026-01-31 13:57:57 +0700

    chore: add keymap to goto file between ateris

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -50,4 +50,4 @@ vim.keymap.set("n", "<leader>q", "<cmd>q!<CR>")
 vim.keymap.set("n", "<leader>ca", "<cmd>%bd|e#|bd#<CR>", { silent = true, desc = "Close others" })
 vim.keymap.set("n", "<leader>c", "<cmd>bp | sp | bn | bd!<CR>")
 
-vim.keymap.set('n', '<S-g>', 'vi*gf')
+vim.cmd("nmap <M-.> vi*gf")
